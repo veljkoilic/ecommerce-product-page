f5cecf8f34826c27b2d37afb523082f4a7f8d83b
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-15 09:59:22 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// - View the optimal layout for the site depending on their device's screen size
+// - See hover states for all interactive elements on the page
+// - Open a lightbox gallery by clicking on the large product image
+// - Switch the large product image by clicking on the small thumbnail images
+// - Add items to the cart
+// - View the cart and remove items from it
+
+
+import { Home } from "./components/Home";
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Home = () => {
+  return (
+    <Container>Ready to start!</Container>
+  )
+}
+
+const Container = styled.div`
+    color: coral;
+    font-size: 20px;
+    padding: 20px;
+`
