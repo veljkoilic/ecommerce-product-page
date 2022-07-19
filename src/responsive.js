72c38e2cfd47c1c230bb05239b2491b1c05eaa49
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-19 14:34:24 +0200

    breakpoint adjustments

--- src/responsive.js
+++ src/responsive.js
@@ -2,21 +2,21 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 375px) {
+    @media only screen and (max-width: 450px) {
       ${props}
     } 
   `;
 };
 export const tablet = (props) => {
   return css`
-    @media only screen and (max-width: 815px) {
+    @media only screen and (max-width: 1000px) {
       ${props}
     }
   `;
 };
 export const laptop = (props) => {
   return css`
-    @media only screen and (max-width: 1440px) {
+    @media only screen and (max-width: 1439px) {
       ${props}
     }
   `;
